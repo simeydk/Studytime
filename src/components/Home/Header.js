5f811da6988e1b4a37d2f8f2c9943d20ad4bf0fa
Ratul Maharaj <RMaharaj3@oldmutual.com>
2020-06-29 03:57:08 +0200

    small screens hot fix

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -1,17 +1,27 @@
-import React from 'react'
-import Logo from "../Logo"
-import './Header.css'
+import React from "react";
+import Logo from "../Logo";
+import { GiHamburgerMenu } from "react-icons/gi";
+import "./Header.css";
 
 function Header() {
-    return (
-        <div className="Header">
-            <Logo/>
-            <div className="Admin">
-                <button className="btn-purple" >Login</button>
-                <button className="btn-purple">Sign Up</button>
-            </div>
-        </div>
-    )
+  const width = window.innerWidth;
+  const breakpoint = 620;
+  
+  return (
+    <div className="Header">
+      <Logo />
+      <div className="Admin">
+        {width < breakpoint ? (
+          <button className="btn-purple">Login</button>
+        ) : (
+          <>
+            <button className="btn-purple">Login</button>
+            <button className="btn-purple">Sign Up</button>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
