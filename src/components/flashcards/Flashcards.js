a5a77ef649c4ac1ea9c386abad81b5cd67f448fc
Ratul Maharaj <RMaharaj3@oldmutual.com>
2020-07-03 01:56:59 +0200

    flip on click

--- src/components/flashcards/Flashcards.js
+++ src/components/flashcards/Flashcards.js
@@ -1,12 +1,16 @@
-import React, { useState } from "react";
-import { MdNavigateNext, MdNavigateBefore } from "react-icons/md";
-import "./Flashcards.css";
+import React, { useState } from "react"
+import { MdNavigateNext, MdNavigateBefore, MdSwapHoriz } from "react-icons/md"
+import "./Flashcards.css"
 
 // max character count of 1567 must be allowed
 
 function Flashcards() {
-  const questions = require("../../../api/flashcards/ExampleQuestions.json");
-  const [ID, setID] = useState(0);
+  const questions = require("../../../api/flashcards/ExampleQuestions.json")
+  const [isFlipped, setIsFlipped] = useState("")
+  const [ID, setID] = useState(0)
+  const handleFlip = e => {
+    isFlipped === "" ? setIsFlipped("flipped") : setIsFlipped("")
+  }
 
   return (
     <>
@@ -14,11 +18,14 @@ function Flashcards() {
         <div className="Content">
           <div className="Intro">
             <h1>Flashcards</h1>
-            <p>Hover over or tap the card to flip it over.</p>
+            <p>Click or touch the card to flip it over.</p>
           </div>
           <div className="flashcard-wrapper">
             <div className="flashcard">
-              <div className="flip-card">
+              <div
+                className={`flip-card ${isFlipped}`}
+                onClick={e => handleFlip()}
+              >
                 <div className="flip-card-inner">
                   <div className="flip-card-front">
                     <h4 style={{ margin: `0` }}>Question</h4>
@@ -26,7 +33,7 @@ function Flashcards() {
                       <h3>{questions.cards[ID].question}</h3>
                     </div>
                     <p style={{ margin: `0`, opacity: `0.6` }}>
-                    {questions.cards[ID].id} of {questions.cards.length}
+                      {questions.cards[ID].id} of {questions.cards.length}
                     </p>
                   </div>
                   <div className="flip-card-back">
@@ -35,17 +42,33 @@ function Flashcards() {
                       <h3>{questions.cards[ID].answer}</h3>
                     </div>
                     <p style={{ margin: `0`, opacity: `0.6` }}>
-                    {questions.cards[ID].id} of {questions.cards.length}
+                      {questions.cards[ID].id} of {questions.cards.length}
                     </p>
                   </div>
                 </div>
               </div>
             </div>
             <div className="flashcard-button-wrapper">
-              <div className="flashcard-button-previous" onClick={(e) => ID-1<0 ? setID(0) : setID(ID - 1 )}>
+              <div
+                className="flashcard-button-previous"
+                onClick={e => (ID - 1 < 0 ? setID(0) : setID(ID - 1))}
+              >
                 <MdNavigateBefore />
               </div>
-              <div className="flashcard-button-next" onClick={(e) => ID+1 >= questions.cards.length ? setID(questions.cards.length-1) : setID(ID + 1 )}>
+              <div
+                className="flashcard-button-flip"
+                onClick={e => handleFlip()}
+              >
+                <MdSwapHoriz />
+              </div>
+              <div
+                className="flashcard-button-next"
+                onClick={e =>
+                  ID + 1 >= questions.cards.length
+                    ? setID(questions.cards.length - 1)
+                    : setID(ID + 1)
+                }
+              >
                 <MdNavigateNext />
               </div>
             </div>
@@ -53,7 +76,7 @@ function Flashcards() {
         </div>
       </div>
     </>
-  );
+  )
 }
 
-export default Flashcards;
+export default Flashcards
